8984ee48c8d0616495c90dfdedaa366ed216471d
DrN <DrN@DESKTOP-GORGVHP>
2018-04-20 17:47:24 -0700

    finished notes

--- COMP_3512_Assignment2/COMP_3512_Assignment2/Tester.cpp
+++ COMP_3512_Assignment2/COMP_3512_Assignment2/Tester.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 
 int main() {
+	//hey Chris, We did all the testing by hand so please refer to the Assignment 2 Testing.docx file in the Resource Files folder of our assignment :)
 	Menu test = Menu();
 	system("pause");
 	return 0;
